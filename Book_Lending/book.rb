620a9aee47c138dead68a74bf3b89ad42c44b2b0
Azbagheri <azadeh.bagheri7@gmail.com>
2017-04-03 23:21:04 -0400

    methods lent_out? and overdue_books are added

--- Book_Lending/book.rb
+++ Book_Lending/book.rb
@@ -3,13 +3,14 @@ class Book
   @@on_shelf = []
   @@on_loan = []
 
+  attr_reader :title, :author, :isbn
   attr_accessor :due_date
 
   def initialize(title, author, isbn)
     @title = title
     @author = author
     @isbn = isbn
-
+   @due_date = nil
   end
 
   def borrow
@@ -26,6 +27,7 @@ end
     if lent_out?
       @@on_loan.delete(self)
       @@on_shelf << self
+      @due_date = nil
       return true
     else
       return false
@@ -33,8 +35,18 @@ end
   end
 
   def lent_out?
-    @@on_loan.include?("#{@title}")
+    @@on_loan.include?(self)
+    # status = @@on_loan.select do |book|
+    #    book.title == self.title
+    #  end
+    #  if status.length == 0
+    #    return false
+    #  else
+    #    return true
+    #  end
+
   end
+
 
 def self.create(title, author, isbn)
   new_book = Book.new(title, author, isbn)
@@ -51,9 +63,14 @@ def self.current_due_date
     year = today.year
     due_date = Time.local(year, month+1, day)
     puts "The current due date is: #{due_date}."
+    return due_date
 end
 
 def self.overdue_books
+    overdues = @@on_loan.select do |book|
+      book.due_date < (Time.now)
+    end
+    return overdues
 end
 
 def self.browse
@@ -64,7 +81,7 @@ def self.available
   @@on_shelf
 end
 
-def borrowed
+def self.borrowed
   @@on_loan
 end
 
